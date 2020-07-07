e9eb94964f3a658f4747c248a5f17a04946ec536
divy-work <dj.srivastava23@gmail.com>
2020-07-07 21:36:08 +0530

    feat: write screenshot

--- _lib/tweet.ts
+++ _lib/tweet.ts
@@ -1,10 +1,10 @@
 import Twitter from "twitter-lite";
 
 const client = new Twitter({
-  consumer_key: "--",
-  consumer_secret: "--",
-  access_token_key: "--",
-  access_token_secret: "--"
+  consumer_key: process.env.consumer_key || "--",
+  consumer_secret: process.env.consumer_secret || "--",
+  access_token_key: process.env.access_token_key || "--",
+  access_token_secret: process.env.access_token_secret || "--"
 });
 
 export async function getTweet() {
--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { getScreenshot } from "./_lib/chromium";
 import { getHtml } from "./_lib/template";
 import { getTweet } from "./_lib/tweet";
+import * as fs from "fs";
 
 (async () => {
   let tweet = await getTweet();
   console.log(tweet)
-  getScreenshot(getHtml(tweet), "png", true);
+  fs.writeFileSync("screenshot.png", getScreenshot(getHtml(tweet), "png", true));
 })()

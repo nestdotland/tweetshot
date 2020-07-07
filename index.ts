5e7c078996efc834446685ac9d6e063f91c313f7
divy-work <dj.srivastava23@gmail.com>
2020-07-07 21:10:19 +0530

    fix: await inside async

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { getScreenshot } from "./_lib/chromium";
 import { getHtml } from "./_lib/template";
 import { getTweet } from "./_lib/tweet";
 
-let tweet = await getTweet();
-
-getScreenshot(getHtml(tweet), "png", true);
+(async () => {
+  let tweet = await getTweet();
+  getScreenshot(getHtml(tweet), "png", true);
+})()

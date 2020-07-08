512deaac905620c848d80482f428e81213003f7c
divy-work <dj.srivastava23@gmail.com>
2020-07-08 11:03:32 +0530

    asd

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
-// import { getScreenshot } from "./_lib/chromium";
 import { getHtml } from "./_lib/template";
 import { getTweet } from "./_lib/tweet";
-// import * as fs from "fs";
 // @ts-ignore
 import toImg from "node-html-to-image";
 

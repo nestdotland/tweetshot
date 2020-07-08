d8c482a727eee5104e6b3052ad1930e275cb1a34
divy-work <dj.srivastava23@gmail.com>
2020-07-08 10:56:19 +0530

    use node-html-to-image

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
-import { getScreenshot } from "./_lib/chromium";
+// import { getScreenshot } from "./_lib/chromium";
 import { getHtml } from "./_lib/template";
 import { getTweet } from "./_lib/tweet";
 import * as fs from "fs";
+import toImg from "node-html-to-image";
 
 (async () => {
   let tweet = await getTweet();
   console.log(tweet)
-  await fs.writeFileSync("screenshot.png", await getScreenshot(getHtml(tweet), "png", true));
+  await toImg({
+    output: "screenshot.png",
+    html: getHtml(tweet)
+  });
 })()

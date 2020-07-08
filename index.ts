85d2de94a6cc72b12971a5a8ec2b312dca69ad78
divy-work <dj.srivastava23@gmail.com>
2020-07-08 10:16:04 +0530

    chores: set dev true

--- index.ts
+++ index.ts
@@ -6,5 +6,5 @@ import * as fs from "fs";
 (async () => {
   let tweet = await getTweet();
   console.log(tweet)
-  await fs.writeFileSync("screenshot.png", await getScreenshot(getHtml(tweet), "png", false));
+  await fs.writeFileSync("screenshot.png", await getScreenshot(getHtml(tweet), "png", true));
 })()
